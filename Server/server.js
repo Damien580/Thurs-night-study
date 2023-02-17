d1a1839aca66d8bde899734fe586afaf9b901c7b
Damien Redd <damiens.redd@gmail.com>
2023-02-16 21:09:43 -0600

    still working on it

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const sors = require('cors')
+const cors = require('cors')
+
 
 const app = express()
 
@@ -7,6 +8,14 @@ app.use(express.json())
 app.use(cors())
 
 const {
-    login,
-    register
-} = require('./controllers/auth')
+    getSpices,
+    addSpice,
+    deleteSpice
+} = require('./controller')
+
+app.get('/api/spices', getSpices)
+app.post('/api/spices', addSpice)
+app.delete('/api/spices/:id', deleteSpice)
+
+
+app.listen(4004, console.log(`App running on port 4000!`))
